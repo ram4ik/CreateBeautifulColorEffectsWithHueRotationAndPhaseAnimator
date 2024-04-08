3ada91b932f0aca1d286f9bbcd2bc74e9de0b16d
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-04-08 09:10:13 +0300

    Initial Commit

---
+++ CreateBeautifulColorEffectsWithHueRotationAndPhaseAnimator/CreateBeautifulColorEffectsWithHueRotationAndPhaseAnimatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateBeautifulColorEffectsWithHueRotationAndPhaseAnimatorApp.swift
+//  CreateBeautifulColorEffectsWithHueRotationAndPhaseAnimator
+//
+//  Created by Ramill Ibragimov on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CreateBeautifulColorEffectsWithHueRotationAndPhaseAnimatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
